5ef1ef779e34177dabb4adec10f5105e10bca4d3
GauravSinghVerma <gaurav.verma18@gmail.com>
2021-03-06 20:21:11 +0530

    Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="30011802/udacityoperationalizeml"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityoperationalizeml --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityoperationalizeml 8080:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="30011802/udacityoperationalizeml"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username 30011802
+docker image tag app:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
